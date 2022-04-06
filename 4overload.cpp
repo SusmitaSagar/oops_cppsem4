90003b7e314e95b074deea962190f4ff0138e9b3
SusmitaSagar_202124 <susmitasagar038@gmail.com>
2022-04-06 08:01:22 +0530

    Update 4overload.cpp

--- 4overload.cpp
+++ 4overload.cpp
@@ -1,6 +1,5 @@
 // program to show that the efeect of default argument 
 //can be alternatively achieved by overloading
-
 #include <iostream>
 using namespace std;
 int power(int x, int y)
@@ -12,26 +11,18 @@ int power(int x, int y)
     }
     return answer;
 }
-
-int power(int x)
-{
+int power(int x){
     int y = 2;
     int answer = 1;
-    for (int i = 1; i <= y; i++)
-    {
+    for (int i = 1; i <= y; i++){
         answer = answer * x;
     }
     return answer;
 }
-
-int main()
-{
+int main(){
     int m, answer;
     int n;
-
     cout << power(3, 4) << endl;
-
     cout << power(3);
-
     return 0;
 }
