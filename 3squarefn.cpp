1aca8484f12569fddc2695c20e576770e7e17bcc
SusmitaSagar_202124 <susmitasagar038@gmail.com>
2022-04-06 07:56:22 +0530

    Update 3squarefn.cpp

--- 3squarefn.cpp
+++ 3squarefn.cpp
@@ -6,15 +6,12 @@ using namespace std;
 double power(double x, int y = 2) // input by user
 {  // this fn will calculate power
     double answer = 1;
-    for (int i = 1; i <= y; i++)
-    {
+    for (int i = 1; i <= y; i++) {
         answer = answer * x;
     }
-
     return answer;
 }
-int main()
-{
+int main(){
     double m, answer;
     int n = 2;
     // int n; // if value is  not default
@@ -22,7 +19,5 @@ int main()
     // cin>>m;  // if n is not default, ask
     cout << power(3, n) << endl;
     cout << power(3, 4);
-
     return 0;
 }
-//////
