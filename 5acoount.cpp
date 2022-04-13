0bc92e4eabd7aa228562dfc39d7487654183b510
SusmitaSagar_202124 <susmitasagar038@gmail.com>
2022-04-13 15:08:03 +0530

    Update 5acoount.cpp

--- 5acoount.cpp
+++ 5acoount.cpp
@@ -1,7 +1,6 @@
 // Write a class Account that reprents your bank account and then use it.
 #include <iostream>
 using namespace std;
-
 class Account
 {
 public:
@@ -11,23 +10,19 @@ int balance = 2000;
         balance = balance-y;
         return balance;
     }
-    
     int amt_depo(int x){
         balance=balance+x;
         return balance;
     }
-
     double intrest (double rate, double time){
       double   simple_interest = balance - (balance*rate*time)/100;
         return simple_interest;
-    }
-};
+    }};
 int main(){
     Account myact ;
    cout<<myact.balance<<endl;
     cout<<myact.amt_witdr(500)<<endl;
    cout<< myact.amt_depo(300)<<endl;
   cout<<  myact.intrest(2,3)<<endl;
-
     return 0;
 }
