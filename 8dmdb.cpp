56356d2262b8744e48cdcfe36eacb50ad09296e9
SusmitaSagar_202124 <susmitasagar038@gmail.com>
2022-05-29 15:13:10 +0530

    Update 8dmdb.cpp

--- 8dmdb.cpp
+++ 8dmdb.cpp
@@ -3,10 +3,8 @@
 
 #include <iostream>
 using namespace std;
-
 class DM
 {
-
 public:
     double meter;
     double centimeter;
@@ -15,17 +13,14 @@ public:
         meter = m;
         centimeter = cm;
     }
-
     void display()
     {
         cout << "The meters are " << meter << endl;
         cout << "The centimeters are " << centimeter << endl;
     }
 };
-
 class DB
 {
-
 public:
     double feet;
     double inch;
@@ -34,39 +29,29 @@ public:
         feet = ft;
         inch = inc;
     }
-
     void display()
     {
         cout << "The feets are " << feet << endl;
         cout << "The inches are " << inch << endl;
     }
-
     friend void add(DM, DB);
 };
-
 void add(DM D1, DB D2)
 {
-
     double R1, R2;
-
     R1 = D1.meter + D2.feet * 0.305;
     R2 = D1.centimeter + D2.inch * 2.54;
     cout << "Meters are " << R1 << endl;
     cout << "Centimeters are " << R2 << endl;
 }
-
 int main()
 {
-
     DM d1;
     d1.getdistance(3, 40);
     d1.display();
-
     DB d2;
     d2.getdistance(6, 24);
     d2.display();
-
     add(d1, d2);
-
     return 0;
 }
